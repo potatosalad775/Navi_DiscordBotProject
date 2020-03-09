2ecefcada11576594266a30fba748496939e9c0d
username <user@email.mail>
2020-03-09 18:20:25 +0900

    added more kick feature varities

--- commands/kick.js
+++ commands/kick.js
@@ -7,7 +7,7 @@
 		}
 		else {
 			const taggedUser = message.mentions.users.first();
-			var rand = Math.floor(Math.random() * 6);
+			var rand = Math.floor(Math.random() * 9);
 
 			switch (rand) {
 				case 0:
@@ -27,6 +27,15 @@
 					break;
 				case 5:
 					message.channel.send(`${taggedUser}을 잡고 싱글렉 테이크다운한 다음, 다스쵸크로 기절시켰습니다.`);
+					break;
+				case 6:
+					message.channel.send(`${taggedUser}는 저 멀리서 날라오는 Navi의 발로 전달된 운동 에너지를 몸소 체험했습니다.`);
+					break;
+				case 7:
+					message.channel.send(`${taggedUser}는 [부당하게 보일 수 있지만, 지극히 의도적인 디스코드 봇 디자인]으로 인해 발길질당했습니다.`);
+					break;
+				case 8:
+					message.channel.send(`묘사가 너무 길어져서 ${taggedUser}가 어떻게 맞았는지 표현할 수가 없네요. 아무튼 발길질당했습니다.`);
 					break;
 			}
 		}
